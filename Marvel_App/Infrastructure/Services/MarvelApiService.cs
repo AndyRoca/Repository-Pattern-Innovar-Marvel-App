8e7c41c727f1792e2aa9c6566feb74d0ab38a6ef
Andres Rivera Castaño <andres.rivera.geo@gmail.com>
2023-08-02 11:56:53 +0200

    Creacion de objeto de la url de marvel API

--- Marvel_App/Infrastructure/Services/MarvelApiService.cs
+++ Marvel_App/Infrastructure/Services/MarvelApiService.cs
@@ -1,7 +1,29 @@
 ﻿
+using System.Net.Http;
+
 namespace Marvel_App.Infrastructure.Services
 {
     public class MarvelApiService
     {
+        string _marvelApiBaseUrl;
+        string _hashParam;
+        string _apiKeyParam;
+        string _timestampParam;
+        string _queryParameters;
+        string _apiEndpoint;
+        HttpClient _client;
+
+        public MarvelApiService(string marvelApiBaseUrl, string hashParam, string apiKeyParam, string timestampParam, string queryParameters, string apiEndpoint, HttpClient client)
+        {
+            _marvelApiBaseUrl = "https://gateway.marvel.com/v1/public/";
+            _hashParam = hashParam;
+            _apiKeyParam = apiKeyParam;
+            _timestampParam = timestampParam;
+            _queryParameters = queryParameters;
+            _apiEndpoint = apiEndpoint;
+        }
+
     }
+
 }
+
